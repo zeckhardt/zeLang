391fe44c86b5d89bffead537f2e6c1b26bd74b11
zeckhardt <zeckhardt9@gmail.com>
2025-05-08 12:32:52 -0400

    add type native function

--- ktInterpreter/src/ze/natives/CoreNatives.kt
+++ ktInterpreter/src/ze/natives/CoreNatives.kt
@@ -14,6 +14,28 @@ object CoreNatives {
             }
 
             override fun toString(): String = "<native fn>"
+        },
+
+        "type" to object : LangCallable {
+            override fun arity(): Int {
+                return 1
+            }
+
+            override fun call(interpreter: Interpreter, arguments: List<Any?>): Any? {
+                val value = arguments.firstOrNull()
+                return when (value) {
+                    null -> "none"
+                    is Double -> "number"
+                    is String -> "string"
+                    is Boolean -> "bool"
+                    is LangCallable -> "function"
+                    else -> "unknown"
+                }
+            }
+
+            override fun toString(): String {
+                return "<native fn type>"
+            }
         }
     )
 
